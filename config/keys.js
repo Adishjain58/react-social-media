e93ce59db2be243a19f11bc236528baf353f4c52
Adishjain58 <adishjain589@gmail.com>
2018-11-12 12:38:52 +0530

    Login Form

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mogoURI: "mongodb://adish:adish123@ds157383.mlab.com:57383/mern-study"
+  mogoURI: "mongodb://adish:adish123@ds157383.mlab.com:57383/mern-study",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,6 +9,9 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //Db config
 
 const db = require("./config/keys").mogoURI;
@@ -18,7 +23,11 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+//Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use routes
 
